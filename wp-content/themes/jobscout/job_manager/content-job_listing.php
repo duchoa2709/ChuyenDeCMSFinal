b5c8acfc36e1ae37aab680328b570bf7f6aa9da7
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-11-21 21:47:12 +0700

    update

--- wp-content/themes/jobscout/job_manager/content-job_listing.php
+++ wp-content/themes/jobscout/job_manager/content-job_listing.php
@@ -70,14 +70,35 @@ $company_name = get_post_meta( get_the_ID(), '_company_name', true );
         </div>
         <div class="p-1 ml-[10px]">
             <h1
-                class=" py-[3px] before:content-[''] before:w-1 before:h-1  before:bg-black before:absolute before:rounded-full before:mt-[7px]  before:-ml-[9px]   text-xs text-black-300 ">
-                Thong tin nguoi dung ung tuyen vi tri job nay</h1>
+                class=" py-[3px] before:content-[''] before:w-1 before:h-1  before:bg-black before:absolute before:rounded-full before:mt-[7px]  before:-ml-[9px] ">
+
+                <?php 
+				do_action( 'job_listing_meta_start' ); 
+
+				if( $job_salary ){
+                    echo '<div class="salary-amt">
+                        <span class="salary text-xs text-black-300">'. ( $job_salary ) .'</span>
+                    </div>';
+                }
+			?>
+
+            </h1>
             <h1
-                class="py-[3px] before:content-[''] before:w-1 before:h-1  before:bg-black before:absolute before:rounded-full before:mt-[7px]  before:-ml-[9px]   text-xs text-black-300 ">
-                Thong tin nguoi dung ung tuyen vi tri job nay</h1>
+                class="py-[3px] before:content-[''] before:w-1 before:h-1  before:bg-black before:absolute before:rounded-full before:mt-[7px]  before:-ml-[9px]   text-xs  ">
+                <div class="company-address">
+                    <p class="text-xs text-black-300"><?php the_job_location( true ); ?> </p>
+                </div>
+            </h1>
             <h1
-                class=" py-[3px] before:content-[''] before:w-1 before:h-1  before:bg-black before:absolute before:rounded-full before:mt-[7px]  before:-ml-[9px]   text-xs text-black-300 ">
-                Thong tin nguoi dung ung tuyen vi tri job nay</h1>
+                class=" py-[3px] before:content-[''] before:w-1 before:h-1  before:bg-black before:absolute before:rounded-full before:mt-[7px]  before:-ml-[9px]  text-black-300   ">
+                <?php
+	                $job_salary = the_job_salary('', '', false);
+	                if ( ! empty( $job_salary ) ) : ?>
+                    <p class="text-xs text-black-300"><?php echo ( $job_salary ); ?></p>
+                    <?php
+	                endif;
+	            ?>
+            </h1>
         </div>
     </div>
 
